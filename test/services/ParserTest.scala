a344e1e716a7ab3d66a2e4a559acfb926bd89734
Paul Durham <pcdurham@googlemail.com>
2017-09-18 09:53:19 +0100

    DDCNLS-1909: multi-threaded validation

--- test/services/ParserTest.scala
+++ test/services/ParserTest.scala
@@ -16,11 +16,14 @@
 
 package services
 
+import java.util.concurrent.atomic.AtomicInteger
+
 import models.{ERSFileProcessingException, SchemeInfo}
 import org.joda.time.DateTime
 import org.scalatest.BeforeAndAfter
-import org.scalatest.concurrent.ScalaFutures
+import org.scalatest.concurrent.{ScalaFutures, Timeouts}
 import org.scalatest.mock.MockitoSugar
+import org.scalatest.time.{Millis, Span}
 import org.scalatestplus.play.{OneServerPerSuite, PlaySpec}
 import play.api.i18n.Messages
 import play.api.mvc.Request
@@ -28,11 +31,18 @@ import services.XMLTestData._
 import services.audit.AuditEvents
 import uk.gov.hmrc.play.http.HeaderCarrier
 import play.api.i18n.Messages.Implicits._
+import uk.gov.hmrc.services.validation.{Cell, DataValidator, Row, ValidationError}
+
+import scala.collection.mutable.ListBuffer
+import scala.concurrent.{Await, Future}
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration.Duration
+import scala.util.Success
 
 /**
   * Created by raghu on 26/01/16.
   */
-class ParserTest extends PlaySpec with OneServerPerSuite with ScalaFutures with MockitoSugar with BeforeAndAfter {
+class ParserTest extends PlaySpec with OneServerPerSuite with ScalaFutures with MockitoSugar with BeforeAndAfter with Timeouts {
 
   object TestDataParser extends DataParser
 
@@ -40,7 +50,21 @@ class ParserTest extends PlaySpec with OneServerPerSuite with ScalaFutures with 
     override val auditEvents: AuditEvents = mock[AuditEvents]
   }
 
-  val schemeInfo: SchemeInfo = SchemeInfo(
+  class MockDataValidator(rowValidator: () => Option[List[ValidationError]]) extends DataValidator {
+    override def validateRows(rows: List[List[String]], contextObjectOpt: Option[AnyRef], firstRowNum: Int, ignoreBlankRows: Boolean): Option[List[ValidationError]] = ???
+    override def validateRowsBuffered(rows: List[List[String]], errorBuffer: ListBuffer[ValidationError], contextObjectOpt: Option[AnyRef], firstRowNum: Int, ignoreBlankRows: Boolean): Unit = ???
+    override def validateRows(rows: List[List[String]], contextObjectOpt: Option[AnyRef], zeroBased: Boolean): Option[List[ValidationError]] = ???
+    override def validateCellBuffered(cell: Cell, errorBuffer: ListBuffer[ValidationError], contextObjectOpt: Option[AnyRef]): Unit = ???
+    override def validateRowBuffered(row: Row, errorBuffer: ListBuffer[ValidationError], contextObjectOpt: Option[AnyRef]): Unit = ???
+    override def validateCell(cell: Cell, contextObjectOpt: Option[AnyRef]): Option[List[ValidationError]] = ???
+    override def validateRow(row: Row, contextObjectOpt: Option[AnyRef]): Option[List[ValidationError]] = rowValidator.apply()
+  }
+
+  private val nl: String = System.lineSeparator()
+  private val timeout = 100
+  private val awaitTimeout = Span(timeout, Millis)
+
+  implicit val schemeInfo: SchemeInfo = SchemeInfo(
     schemeRef = "XA11999991234567",
     timestamp = DateTime.now,
     schemeId = "123PA12345678",
@@ -49,6 +73,7 @@ class ParserTest extends PlaySpec with OneServerPerSuite with ScalaFutures with 
     schemeType = "EMI"
   )
 
+  implicit val schemeName = schemeInfo.schemeName
   implicit val hc: HeaderCarrier = mock[HeaderCarrier]
   implicit val request: Request[_] = mock[Request[_]]
 
@@ -120,4 +145,146 @@ class ParserTest extends PlaySpec with OneServerPerSuite with ScalaFutures with 
     }
     result.message mustBe Messages("ers.exceptions.dataParser.incorrectSheetName")
   }
+
+  private def getRowsFromFileFixture = new {
+    val content = s"field1, field2, field3${nl}field1${nl}field1, field2"
+    val sheetName = "Other_Options_V3.csv"
+  }
+
+  "fetch all rows from the CSV file" in {
+    val fixture = getRowsFromFileFixture
+    val iterator = fixture.content.split(nl).iterator
+    val actual = TestDataGenerator.getRowsFromFile(iterator)
+    val expected: List[List[String]] = fixture.content.split(nl).toList.map(s => s.split(",").toList)
+    actual mustBe expected
+  }
+
+  "handle empty CSV files" in {
+    val fixture = getRowsFromFileFixture
+    val actual = TestDataGenerator.getRowsFromFile(Iterator.empty)
+    actual.isEmpty mustBe true
+  }
+
+  "return the correct number of chunks" in {
+    // scalastyle:off magic.number
+    TestDataGenerator.numberOfChunks(0, 1) mustBe 0
+    TestDataGenerator.numberOfChunks(1, 2) mustBe 1
+    TestDataGenerator.numberOfChunks(2, 2) mustBe 1
+    TestDataGenerator.numberOfChunks(9, 2) mustBe 5
+    // scalastyle:on magic.number
+  }
+
+  private def submitChunksFixture = new {
+    val rows = List(
+      List("field1", "field2"),
+      List("field1", "field2"),
+      List("field1", "field2"),
+      List("field1", "field2"),
+      List("field1", "field2")
+    )
+  }
+
+  "submit the correct number of futures" in {
+    val fixture = submitChunksFixture
+
+    val rowValidator = () => Some(List.empty[ValidationError])
+    val dataValidator = new MockDataValidator(rowValidator)
+
+    val actual = TestDataGenerator.submitChunks(fixture.rows, 3, 2, 3, dataValidator)
+    actual.length mustBe 3
+  }
+
+  "process all rows" in {
+    val fixture = submitChunksFixture
+    val count: AtomicInteger = new AtomicInteger(0)
+
+    val rowValidator = () => {
+      count.incrementAndGet()
+      None
+    }
+
+    val dataValidator = new MockDataValidator(rowValidator)
+
+    val actual = TestDataGenerator.submitChunks(fixture.rows, 3, 2, 3, dataValidator)
+
+    for (future <- actual) {
+      failAfter(awaitTimeout) {
+        Await.ready(future, Duration.Inf)
+      }
+    }
+
+    count.get() mustBe 5
+  }
+
+  private def processChunkFixture = new {
+    val chunk = List(
+      List("field1", "field2"),
+      List("field1", "field2"),
+      List("field1", "field2")
+    )
+  }
+
+  "process each row in the chunk" in {
+    val fixture = processChunkFixture
+    var count: Int = 0
+
+    val rowValidator = () => {
+      count += 1
+      None
+    }
+
+    val dataValidator = new MockDataValidator(rowValidator)
+
+    TestDataGenerator.processChunk(fixture.chunk, 1, 3, dataValidator)
+    count mustBe fixture.chunk.size
+  }
+
+  "throw exception when CSV file contains errors" in {
+    val fixture = processChunkFixture
+
+    val rowValidator = () => {
+      Some(List(ValidationError(Cell("AA", 1, "bad"), "bad data", "bad cell AA", "cell AA is bad")))
+    }
+
+    val dataValidator = new MockDataValidator(rowValidator)
+
+    intercept[ERSFileProcessingException] {
+      TestDataGenerator.processChunk(fixture.chunk, 1, 3, dataValidator)
+    }
+  }
+
+  "aggregate chunk results corectly" in {
+    val row1 = List(Seq("field1", "field2"))
+    val row2 = List.empty[Seq[String]]
+    val row3 = List(Seq("field3", "field4", "field5"))
+
+    val submissions: Array[Future[List[Seq[String]]]] = Array(
+      Future(row1),
+      Future(row2),
+      Future(row3)
+    )
+
+    val expected = Some(Success(List(row1, row2, row3).flatten))
+    val actual = TestDataGenerator.getResult(submissions)
+
+    failAfter(awaitTimeout) {
+      Await.ready(actual, Duration.Inf)
+    }
+
+    actual.value mustBe expected
+  }
+
+  "aggregate to an empty list when there are no chunk results" in {
+    val submissions = Array.empty[Future[List[Seq[String]]]]
+
+    val expected = Some(Success(List.empty[Seq[String]]))
+    val actual = TestDataGenerator.getResult(submissions)
+
+    failAfter(awaitTimeout) {
+      Await.ready(actual, Duration.Inf)
+    }
+
+    actual.value mustBe expected
+  }
+
 }
