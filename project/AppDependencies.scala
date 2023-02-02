621c15f69fbd567fb89d652ab9bc31b20296b66d
Matthew Ward <96430760+matthewwardcap@users.noreply.github.com>
2023-02-02 09:38:09 +0000

    [CR][DDCE-3497] dependency update

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,18 +5,18 @@ object AppDependencies {
 
   val silencerVersion = "1.7.12"
   val bootstrapVersion = "7.13.0"
-  val akkaVersion = "2.7.0"
+  val akkaVersion = "2.6.20"
 
   val compile: Seq[ModuleID] = Seq(
     ws,
-    "com.lightbend.akka"     %% "akka-stream-alpakka-csv"    % "5.0.0",
+    "com.lightbend.akka"     %% "akka-stream-alpakka-csv"    % "3.0.4",
     "com.typesafe.play"      %% "play-json-joda"             % "2.9.4",
     "com.typesafe.akka"      %% "akka-stream"                % akkaVersion,
     "com.typesafe.akka"      %% "akka-slf4j"                 % akkaVersion,
     "com.typesafe.akka"      %% "akka-protobuf"              % akkaVersion,
     "com.typesafe.akka"      %% "akka-actor-typed"           % akkaVersion,
     "com.typesafe.akka"      %% "akka-serialization-jackson" % akkaVersion,
-    "com.typesafe.akka"      %% "akka-http-spray-json"       % "10.4.0",
+    "com.typesafe.akka"      %% "akka-http-spray-json"       % "10.2.10",
     "org.scala-lang.modules" %% "scala-xml"                  % "1.3.0",
     "uk.gov.hmrc"            %% "bootstrap-backend-play-28"  % bootstrapVersion,
     "uk.gov.hmrc"            %% "domain"                     % "8.1.0-play-28",
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -2,9 +2,7 @@ resolvers += "HMRC-open-artefacts-maven" at "https://open.artefacts.tax.service.
 resolvers += Resolver.url("HMRC-open-artefacts-ivy",url("https://open.artefacts.tax.service.gov.uk/ivy2"))(Resolver.ivyStylePatterns)
 resolvers += "Typesafe Releases" at "https://repo.typesafe.com/typesafe/releases/"
 
-addSbtPlugin("uk.gov.hmrc" % "sbt-auto-build" % "3.6.0")
-
-addSbtPlugin("uk.gov.hmrc" % "sbt-git-versioning" % "2.2.0")
+addSbtPlugin("uk.gov.hmrc" % "sbt-auto-build" % "3.9.0")
 
 addSbtPlugin("com.github.gseitz" % "sbt-release" % "1.0.10")
 
@@ -14,6 +12,6 @@ addSbtPlugin("org.scoverage" % "sbt-scoverage" % "2.0.0")
 
 addSbtPlugin("org.scalastyle" %% "scalastyle-sbt-plugin" % "1.0.0")
 
-addSbtPlugin("uk.gov.hmrc" % "sbt-distributables" % "2.1.0")
+addSbtPlugin("uk.gov.hmrc" % "sbt-distributables" % "2.2.0")
 
 addSbtPlugin("com.timushev.sbt" % "sbt-updates" % "0.6.3")
