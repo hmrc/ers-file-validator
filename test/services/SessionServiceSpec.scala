0c155422e92b48f38c6da469b8b5044b019f581f
Paul Callaghan <83694362+PaulC-TDS@users.noreply.github.com>
2023-12-07 16:08:27 +0000

    DDCE-4690: Changes to remove http caching (#58)
    * DDCE-4690: Changes for removing http caching with failing tests
    
    * DDCE-4690: Updating test coverage and adding IT tests
    
    * DDCE-4690: Addressing PR bot feedback
    
    * DDCE-4690: Added due to failing PR builder
    
    * DDCE-4690: Removing previous change
    
    * DDCE-4690: Changes following PR review

--- test/services/SessionServiceSpec.scala
+++ test/services/SessionServiceSpec.scala
@@ -16,9 +16,8 @@
 
 package services
 
-import config.ERSFileValidatorSessionCache
 import models.upscan.UpscanCallback
-import org.mockito.ArgumentMatchers.any
+import org.mockito.ArgumentMatchers.{any, anyString}
 import org.mockito.Mockito._
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatestplus.mockito.MockitoSugar
@@ -26,32 +25,34 @@ import org.scalatestplus.play.PlaySpec
 import play.api.libs.json._
 import play.api.mvc.AnyContentAsEmpty
 import play.api.test.FakeRequest
-import uk.gov.hmrc.http.HeaderCarrier
-import uk.gov.hmrc.http.cache.client.CacheMap
+import repository.ERSFileValidatorSessionRepository
+import uk.gov.hmrc.http.{HeaderCarrier, SessionKeys}
+import uk.gov.hmrc.mongo.cache.DataKey
 
-import scala.language.postfixOps
+import java.util.UUID
 import scala.concurrent.duration._
 import scala.concurrent.{Await, ExecutionContext, ExecutionContextExecutor, Future}
+import scala.language.postfixOps
 
 class SessionServiceSpec extends PlaySpec with ScalaFutures with MockitoSugar {
 
-  val mockSessionCache: ERSFileValidatorSessionCache = mock[ERSFileValidatorSessionCache]
+  val sessionPair: (String, String) = SessionKeys.sessionId -> UUID.randomUUID.toString
+  val mockSessionCache: ERSFileValidatorSessionRepository = mock[ERSFileValidatorSessionRepository]
   implicit val ec: ExecutionContextExecutor = ExecutionContext.global
-  val sessionService = new SessionService(mockSessionCache, ec)
+  val sessionService = new SessionCacheService(mockSessionCache, ec)
   implicit val request: FakeRequest[AnyContentAsEmpty.type] = FakeRequest()
   val hc: HeaderCarrier = HeaderCarrier()
+  val CALLBACK_DATA_KEY = "callback_data_key"
 
   "storeCallbackData" must {
     "successfully store attachments callback post data" in {
       val postData: UpscanCallback = UpscanCallback("thefilename", "downloadUrl", Some(1000L))
 
-      val json = Json.toJson[UpscanCallback](postData)
-      when(mockSessionCache.cache[UpscanCallback]
-        (any[String], any[UpscanCallback])
-        (any[Writes[UpscanCallback]], any[HeaderCarrier], any()))
-        .thenReturn(Future.successful(CacheMap("sessionValue", Map(sessionService.CALLBACK_DATA_KEY -> json))))
+      when(mockSessionCache.putSession(DataKey(anyString()), any())
+      (any(), any(), any()))
+        .thenReturn(Future.successful(sessionPair))
 
-      val result: Option[UpscanCallback] = Await.result(sessionService.storeCallbackData(postData, 1000)(hc), 10 seconds)
+      val result: Option[UpscanCallback] = Await.result(sessionService.storeCallbackData(postData, 1000)(request), 10 seconds)
 
       result.get.length must be(Some(1000L))
       result.get.noOfRows must be (Some(1000))
@@ -60,12 +61,12 @@ class SessionServiceSpec extends PlaySpec with ScalaFutures with MockitoSugar {
     "return a None when cache can't be returned" in {
       val postData: UpscanCallback = UpscanCallback("thefilename", "downloadUrl", Some(1000L))
 
-      when(mockSessionCache.cache[UpscanCallback]
-        (any[String], any[UpscanCallback])
-        (any[Writes[UpscanCallback]], any[HeaderCarrier], any()))
+      when(mockSessionCache.putSession
+      (DataKey(anyString()), any())
+        (any(), any(), any()))
         .thenReturn(Future.failed(new Exception("")))
 
-      val result: Option[UpscanCallback] = Await.result(sessionService.storeCallbackData(postData, 1000)(hc), 10 seconds)
+      val result: Option[UpscanCallback] = Await.result(sessionService.storeCallbackData(postData, 1000)(request), 10 seconds)
       result mustBe None
     }
   }
