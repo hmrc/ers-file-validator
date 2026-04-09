0249ed6ab06c2b7068097462ae8ade347454e369
alisajadg191 <34246449+alisajadg191@users.noreply.github.com>
2026-04-09 13:25:31 +0100

    DDCE-8236: Updated Bootstrap version, fixed broken test cases upto 100%

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -12,7 +12,7 @@ object AppDependencies {
     "org.scala-lang.modules" %% "scala-xml"                 % "2.4.0",
     "uk.gov.hmrc"            %% "bootstrap-backend-play-30" % bootstrapVersion,
     "uk.gov.hmrc"            %% "domain-play-30"            % "11.0.0",
-    "commons-codec"           % "commons-codec"             % "1.19.0",
+    "commons-codec"           % "commons-codec"             % "1.21.0",
     "uk.gov.hmrc.mongo"      %% "hmrc-mongo-play-30"        % mongoVersion,
     "org.apache.pekko"       %% "pekko-connectors-csv"      % pekkoVersion,
     "uk.gov.hmrc"            %% "ers-file-validator-config" % "0.12.0"
